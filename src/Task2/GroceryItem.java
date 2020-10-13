1ce17ec1fe6337d9926154fc7b3ffc0e9fc0bb61
Muskaan0111 <parmarmuskaan27@gmail.com>
2020-10-13 13:13:13 +0530

    Task2

---
+++ src/Task2/GroceryItem.java
@@ -0,0 +1,12 @@
+package Task2;
+
+public class GroceryItem {
+    String name;
+    int pricePerKg;
+
+
+    public GroceryItem(String name, int pricePerKg) {
+        this.name = name;
+        this.pricePerKg = pricePerKg;
+    }
+}
